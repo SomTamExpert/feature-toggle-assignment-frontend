a6977fe3a96809929d7d2d94ccc09fada1ee32dc
Marco Karpf <2marco.karpf2@gmail.com>
2022-10-04 16:47:11 +0200

    document methods in feature toggle service

--- src/services/feature-toggle.service.ts
+++ src/services/feature-toggle.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of, tap} from 'rxjs';
+import {Observable} from 'rxjs';
 import {FeatureToggle} from "../models/feature-toggles.model";
 import {MessageService} from "./message.service";
 
@@ -15,51 +15,77 @@ export class FeatureToggleService {
   constructor(private messageService: MessageService, private httpClient: HttpClient) {
   }
 
-  private log(message: String): void {
-    this.messageService.addMessage(`featureToggleervice'${message}`);
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error); // log to console instead
-      this.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
-  }
-
+  /**
+   * Get all featureToggles from the server
+   * @returns {Observable<FeatureToggle[]>}
+   */
   getFeatureToggles(): Observable<FeatureToggle[]> {
     console.log("fetching all featureToggle")
     return this.httpClient.get<FeatureToggle[]>(this.baseURL);
   }
 
+  /**
+   * get featureToggle by id
+   * @param id
+   * @returns {Observable<FeatureToggle>}
+   */
   getFeatureToggle(id: number): Observable<FeatureToggle> {
     return this.httpClient.get<FeatureToggle>(this.baseURL + "/" + id);
   }
 
+  /**
+   * create a new featureToggle
+   * @param FeatureToggle
+   * @returns {Observable<FeatureToggle>}
+   */
   createFeatureToggle(FeatureToggle: FeatureToggle): Observable<FeatureToggle> {
     return this.httpClient.post<FeatureToggle>(this.baseURL, FeatureToggle);
   }
 
+  /**
+   * update featureToggle
+   * @param id
+   * @param FeatureToggle
+   * @returns {Observable<FeatureToggle>}
+   */
   updateFeatureToggle(id: number | undefined, FeatureToggle: FeatureToggle,): Observable<FeatureToggle> {
     return this.httpClient.put<FeatureToggle>(this.baseURL + "/" + id, FeatureToggle);
   }
 
+  /**
+   * delete featureToggle by id
+   * @param id
+   * @returns {Observable<any>}
+   */
   deleteFeatureToggle(id: number | undefined): Observable<any> {
     return this.httpClient.delete(this.baseURL + "/" + id);
   }
 
+  /**
+   * get all active featureToggles
+   * @returns {Observable<FeatureToggle[]>}
+   */
   getActiveFeatureToggles(): Observable<FeatureToggle[]> {
     console.log("fetching all active featureToggle")
     return this.httpClient.get<FeatureToggle[]>(this.baseURL + "/active")
 
   }
 
+  /**
+   * search featureToggle by name
+   * @param name
+   * @returns {Observable<FeatureToggle[]>}
+   */
   findFeatureToggleByName(name: string): Observable<FeatureToggle[]> {
     console.log("fetching all featureToggle by name")
     return this.httpClient.get<FeatureToggle[]>(`${this.baseURL}?name=${name}`)
 
   }
 
+  /**
+   * remove all featureToggles
+   * @returns {Observable<any>}
+   */
   removeAllFeatureToggles(): Observable<any> {
     return this.httpClient.delete(this.baseURL);
     console.log("delete all featureToggle")
